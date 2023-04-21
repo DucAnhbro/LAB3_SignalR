20309a89c48db72c6fdaa018e944ae2d767b4cec
NTQ-SOLUTION\anh.luu1 <anh.luu1@ntq-solution.com.vn>
2023-04-21 17:00:49 +0700

    Add project files.

---
+++ Lab3/Models/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Lab3.Models
+{
+    public partial class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Products> Products { get; set; }
+       
+    }
+}
